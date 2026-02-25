67f51d68f45ce0eef174ff9c7d07d89cdf8c52f6
kamawebdev13 <karinadatanalyst13@alumnos.cei.es>
2026-02-25 21:01:55 +0100

    feat: implement Karina component and update App to include it

---
+++ src/componentes/Karina.tsx
@@ -0,0 +1,23 @@
+
+
+const Karina = () => {
+//DATOS PERSONALES 
+    const name:string = "Karina"
+    const age:number = 45
+    const jobs:string[] = ["graphic designer", "architect", "maintenance engineer", "English teacher"]
+
+    return (
+        <>
+        <div>{name}</div>
+        <div>{age}</div>
+        <div>{jobs}</div>
+        <img src="{reactSVG}" alt="mi fotito" />
+        </>
+
+
+
+        
+    )
+
+}
+export default Karina
